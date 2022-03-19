581454b2bc681157f49bd7e30f4f7620deb7d372
Duncan Schoenmakers <400156@student.fontys.nl>
2022-03-19 15:20:35 +0100

    initial commit

---
+++ src/test/java/com/energy/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.energy.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
